4c9eb50942d9d04119989df244a39d53557de5b6
Ergun-Deniz-Buyruk <ergunndenizbuyruk@gmail.com>
2021-11-07 00:03:47 +0300

    View binding eklendi.

--- GeoQuiz/app/src/main/java/com/ergundenizbuyruk/geo_quiz/MainActivity.java
+++ GeoQuiz/app/src/main/java/com/ergundenizbuyruk/geo_quiz/MainActivity.java
@@ -3,12 +3,19 @@ package com.ergundenizbuyruk.geo_quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.ergundenizbuyruk.geo_quiz.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
